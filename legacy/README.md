d46d5ad7436a6efae6542dd24cb9d32617ff84e4
Evan Crows <evan.of.crows@gmail.com>
2019-06-19 01:41:38 +0000

    Removing legacy code.

--- legacy/README.md
+++
@@ -1,1 +0,0 @@
-# l2-reddit-experiment
--- legacy/view_layers.py
+++
@@ -1,3 +0,0 @@
-from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
-
-print_tensors_in_checkpoint_file("gs://redbert/validation-models/1560486820809_unmasked_seed44_fold1of10/model.ckpt-500", tensor_name='output_weights', all_tensors=False)
